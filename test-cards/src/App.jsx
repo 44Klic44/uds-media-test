886fc292b586a5df63f1bb5b8970edc7c8ed3747
ROMAN TYSACHNYY <stance_srt@mail.ru>
2025-11-18 22:18:22 +0300

    fonts

--- test-cards/src/App.jsx
+++ test-cards/src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './styles/global.scss'; 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-   <p>npm run dev для запуска</p>
+     <h1 className="text-h1">Euclid Flex Bold — Заголовок</h1>
+      <p className="text-p">Euclid Flex Medium — Параграф</p>
+      <button className="btn">Roboto Regular — Кнопка</button>
     </>
   )
 }
