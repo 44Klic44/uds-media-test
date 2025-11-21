89df8024cde5a40147b014bef9e9ba90bdd5927a
ROMAN TYSACHNYY <stance_srt@mail.ru>
2025-11-21 20:36:22 +0300

    apptiv-style

--- test-cards/src/App.jsx
+++ test-cards/src/App.jsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import './styles/global.scss'; 
 import { cards } from './data/udsPlaces';
 import Sprite from './components/Sprite/Sprite';
-import Icon from './components/icon/Icon';
+
 import UdsCard from "./components/Card/UdsCard"; 
 
 function App() {
